66efc3e81b42b7f53111e40cca2cc4f220d61ebf
chu-young <ttheko101@gmail.com>
2021-12-11 09:55:46 +0200

    added new nav js and css

---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar(){
+
+return(
+
+    <nav>
+    <ul>
+       <li class="logo">CodingNepal</li>
+       <li class="items"><a href="#">Home</a></li>
+       <li class="items"><a href="#">About</a></li>
+       <li class="items"><a href="#">Blogs</a></li>
+       <li class="items"><a href="#">Contact</a></li>
+       <li class="items"><a href="#">Feedback</a></li>
+       <li class="btn"><a href="#"><i class="fas fa-bars"></i></a></li>
+    </ul>
+ </nav>
+
+);
+
+}
+
+export default Navbar
