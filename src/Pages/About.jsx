babe0fd8e7eeda168c0d63964030a0aa5e96b3fa
chu-young <ttheko101@gmail.com>
2021-12-16 10:48:38 +0200

    added structure

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,21 @@
+import styles from "../CSS Files/About.module.css"
+
 function About(){
 
 return(
 
 <section>
 
+<div className={styles.about}>
+
+<div className={styles.aboutText}>
 <h1>About Us</h1>
+
+</div>
+
+<div className={styles.aboutImage}></div>
+
+</div>
 
 </section>
 
